a78af5d175c788ceb78d3833a2ea45d34a2f2eee
Depurk <2depurk@gmail.com>
2021-10-31 14:01:37 +0530

    added search,filter,pagination

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const Errorhandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const Apifeatures = require("../utils/apifeatures");
 // create product- admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -12,8 +13,15 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 //Get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
-  res.status(200).json({ success: true, products });
+  const resultPerPage = 5;
+  const productCount = await Product.countDocuments();
+
+  const apiFeature = new Apifeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(resultPerPage);
+  const products = await apiFeature.query;
+  res.status(200).json({ success: true, products, productCount });
 });
 //Get product details
 exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
