7f7e642b5201d48f33fb2987e9746fe583df2d62
Depurk <2depurk@gmail.com>
2021-11-02 13:52:28 +0530

    user login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,7 @@
 const Errorhandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const User = require("../models/userModel");
+const sendToken = require("../utils/jwtToken");
 
 // Register user
 
@@ -16,10 +17,31 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJWTToken();
+  sendToken(user, 201, res);
+});
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
+// login user
+
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if both email and password provided
+
+  if (!email || !password) {
+    return next(new Errorhandler("Please enter email and password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new Errorhandler("Email or password invalid", 401));
+  }
+
+  const isPasswordMatched = user.comparePassword(password);
+
+  if (!isPasswordMatched) {
+    return next(new Errorhandler("Email or password invalid", 401));
+  }
+
+  sendToken(user, 200, res);
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -56,4 +56,10 @@ userSchema.methods.getJWTToken = function () {
   });
 };
 
+// compare password
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", userSchema);
