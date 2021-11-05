1921bf1e93a19d09b0c6e9604f45f8ef2833b110
Depurk <2depurk@gmail.com>
2021-11-05 12:01:19 +0530

    updating errorhandler

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -11,6 +11,25 @@ module.exports = (err, req, res, next) => {
     err = new Errorhandler(message, 400);
   }
 
+  //mongoose duplicate key error
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+    err = new Errorhandler(message, 400);
+  }
+
+  //wrong JWT error
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json web token is invalid, try again`;
+    err = new Errorhandler(message, 400);
+  }
+
+  //JWT expire error
+
+  if (err.name === "TokenExpiredError") {
+    const message = `Json web token is expired, try again`;
+    err = new Errorhandler(message, 400);
+  }
+
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
