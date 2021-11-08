757c4f05e93a19550da5a796f507d198a54d466d
Depurk <2depurk@gmail.com>
2021-11-08 13:15:22 +0530

    user password update implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -135,3 +135,32 @@ exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save();
   sendToken(user, 200, res);
 });
+
+//get user details
+
+exports.getUserDetails = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id);
+
+  res.status(200).json({
+    success: true,
+    user,
+  });
+});
+
+//update user password
+
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isPasswordMatched) {
+    return next(new Errorhandler("Old password is incorrect", 400));
+  }
+  if (req.body.newPassword !== req.body.confirmPassword) {
+    return next(new Errorhandler("Passwords do not match", 400));
+  }
+  user.password = req.body.newPassword;
+  
+  await user.save();
+
+  sendToken(user, 200, res);
+});
